2aebd1afefbb509b637e7ec65798eeb5a9c291cd
Jonathan Chen <jonathan.shaoming.chen@gmail.com>
2022-08-17 09:01:34 -0700

    Adding files to GitHub

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+var greetings = "Hello, and welcome to a virtual team tour.";
+var team = "Minnesota Vikings";
+var name = "Jonny"
+var stadium = "US Bank Stadium"
+
+// TODO: Create 3-Integer variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+var age = 82;
+var year = 1960;
+var championships = 0;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+var players = ["Justin Jefferson", "Dalvin Cook", "Kirk Cousins", "Danielle Hunter", "Eric Kendricks", "Harrison Smith"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+var boolean = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log(greetings);
+console.log("Today, you will be touring the " + team + ".");
+console.log("My name is " + name + ", and I will be your tour guide." + "\n");
+console.log("The " + team + " are " + age + " years old, being founded in " + year + "."); 
+console.log("However, to the dismay of many fans, they have won " + championships + " championships." + "\n");
+console.log("The current team consists of players such as " + players[0] + " and " + players[4] + " among many others." + "\n");
+console.log("It is " + boolean + " that many fans have high hopes for these upcoming few seasons, due to a new head coach and general manager being hired.");
+console.log("Let's hope that this team can bring a trophy to " + stadium + " soon!");
